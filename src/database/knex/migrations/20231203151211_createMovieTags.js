9528f8df15f36c418901ddcff04b1c9b7e11a4dc
carolpascott <carolpascott01@gmail.com>
2023-12-03 16:26:20 -0300

    criação das tabelas pelo knex

---
+++ src/database/knex/migrations/20231203151211_createMovieTags.js
@@ -0,0 +1,8 @@
+exports.up = knex => knex.schema.createTable("movie_tags", table => {
+    table.increments("id");
+    table.integer("note_id").references("id").inTable("movie_notes");
+    table.integer("user_id").references("user_id").inTable("movie_notes");
+    table.text("name");
+});
+
+exports.down = knex => knex.schema.dropTable("movie_tags");
