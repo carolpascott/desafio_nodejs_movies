e807625f14e0c172d416e9effa70a0ef75200754
carolpascott <carolpascott01@gmail.com>
2023-12-04 22:02:13 -0300

    tratamento de erros com try (compatível com knex

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,31 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
+
+        try {
+            const checkUserExist = await knex("users")
+                .where({ email: email })
+                .first();
+    
+            if(checkUserExist) { 
+                throw new AppError("Este email já está cadastrado!")
+            }    
+        }
+
+        catch(error) {
+            return response.status(400).json({ error });
+        }
         
-        response.json({ name, email, password });
+       await knex("users").insert({
+            name,
+            email,
+            password
+        })
+        
+        return response.status(201).json({ name, email, password });        
     }
 }
 
